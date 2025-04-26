678fdc8bb34239b79026092e79baca6883d147d8
Skjaere <korvusz@proton.me>
2025-04-26 17:30:03 +0200

    fix: respect x-ratelimit-after header on 429 responses from TorBox, and increasing rate limit to 60/hour

---
+++ src/main/kotlin/io/skjaere/debridav/debrid/client/torbox/TorBoxHttpClientConfiguration.kt
@@ -0,0 +1,73 @@
+package io.skjaere.debridav.debrid.client.torbox
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.plugin
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import io.skjaere.debridav.configuration.DebridavConfigurationProperties
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.serialization.json.Json
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+private const val LOCK_WAIT_POLL_TIME = 500L
+
+private const val ONE_SECOND = 1000L
+
+@Configuration
+@ConditionalOnExpression("#{'\${debridav.debrid-clients}'.contains('torbox')}")
+class TorBoxHttpClientConfiguration(
+    private val debridavConfigurationProperties: DebridavConfigurationProperties
+) {
+    val lock = Mutex()
+    private val logger = LoggerFactory.getLogger(TorBoxHttpClientConfiguration::class.java)
+
+    @Bean
+    fun torboxHttpClient(): HttpClient {
+        val client = HttpClient(CIO) {
+            install(HttpTimeout) {
+                connectTimeoutMillis = debridavConfigurationProperties.connectTimeoutMilliseconds
+                requestTimeoutMillis = debridavConfigurationProperties.readTimeoutMilliseconds
+            }
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+
+        client.plugin(HttpSend).intercept { request ->
+            if (lock.isLocked) {
+                do {
+                    delay(LOCK_WAIT_POLL_TIME)
+                } while (lock.isLocked)
+            }
+            val originalCall = execute(request)
+            if (originalCall.response.status == HttpStatusCode.TooManyRequests) {
+                var result = originalCall
+                lock.withLock {
+                    do {
+                        val waitMs = (result.response.headers["x-ratelimit-after"]?.toLong() ?: 1L) * ONE_SECOND
+                        logger.info("Waiting $waitMs milliseconds...")
+                        delay(waitMs)
+                        result = execute(request)
+                    } while (result.response.status == HttpStatusCode.TooManyRequests)
+                }
+                result
+            } else originalCall
+        }
+        return client
+    }
+}
