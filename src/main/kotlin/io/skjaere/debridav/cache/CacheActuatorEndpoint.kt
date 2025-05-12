5bc3dc80d401a9c50a00b9b2288f8d2cdb39fa7e
Skjaere <korvusz@proton.me>
2025-05-12 21:03:32 +0200

    fix: adding cache purging endpoint

---
+++ src/main/kotlin/io/skjaere/debridav/cache/CacheActuatorEndpoint.kt
@@ -0,0 +1,17 @@
+package io.skjaere.debridav.cache
+
+
+import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint
+import org.springframework.stereotype.Component
+
+@Component
+@Endpoint(id = "cache")
+class CacheActuatorEndpoint(
+    private val fileChunkCachingService: FileChunkCachingService
+) {
+    @DeleteOperation
+    fun purgeCache() {
+        fileChunkCachingService.purgeCache()
+    }
+}
